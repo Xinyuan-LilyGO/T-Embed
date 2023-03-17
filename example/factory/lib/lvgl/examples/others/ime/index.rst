0ec5d333f8b714c8865f89928d56646246076714
lewishe <lewishe@outlook.com>
2023-03-17 19:48:59 +0800

    Update example,Clear redundancy

--- example/factory/lib/lvgl/examples/others/ime/index.rst
+++
@@ -1,8 +0,0 @@
-
-Simple Pinyin IME example 
-"""""""""""""""""""
-
-.. lv_example:: others/ime/lv_example_ime_pinyin_1
-  :language: c
-
-
--- example/factory/src/main.cpp
+++ example/factory/src/main.cpp
@@ -42,29 +42,6 @@ QueueHandle_t play_music_queue;
 QueueHandle_t play_time_queue;
 static EventGroupHandle_t lv_input_event;
 
-typedef struct {
-  uint8_t cmd;
-  uint8_t data[14];
-  uint8_t len;
-} lcd_cmd_t;
-
-lcd_cmd_t lcd_st7789v[] = {
-    {0x11, {0}, 0 | 0x80},
-    // {0x3A, {0X06}, 1},
-    {0xB2, {0X0B, 0X0B, 0X00, 0X33, 0X33}, 5},
-    {0xB7, {0X75}, 1},
-    {0xBB, {0X28}, 1},
-    {0xC0, {0X2C}, 1},
-    {0xC2, {0X01}, 1},
-    {0xC3, {0X1F}, 1},
-    {0xC6, {0X13}, 1},
-    {0xD0, {0XA7}, 1},
-    {0xD0, {0XA4, 0XA1}, 2},
-    {0xD6, {0XA1}, 1},
-    {0xE0, {0XF0, 0X05, 0X0A, 0X06, 0X06, 0X03, 0X2B, 0X32, 0X43, 0X36, 0X11, 0X10, 0X2B, 0X32}, 14},
-    {0xE1, {0XF0, 0X08, 0X0C, 0X0B, 0X09, 0X24, 0X2B, 0X22, 0X43, 0X38, 0X15, 0X16, 0X2F, 0X37}, 14},
-
-};
 
 void ui_task(void *param);
 void wav_task(void *param);
@@ -190,27 +167,8 @@ void ui_task(void *param) {
   static lv_color_t *buf1, *buf2;
 
   tft.begin();
-  for (uint8_t i = 0; i < (sizeof(lcd_st7789v) / sizeof(lcd_cmd_t)); i++) {
-    tft.writecommand(lcd_st7789v[i].cmd);
-    for (int j = 0; j < lcd_st7789v[i].len & 0x7f; j++) {
-      tft.writedata(lcd_st7789v[i].data[j]);
-    }
-
-    if (lcd_st7789v[i].len & 0x80) {
-      delay(120);
-    }
-  }
-
   tft.setRotation(3);
   tft.fillScreen(TFT_BLACK);
-
-  // ledcSetup(0, 10000, 8);
-  // ledcAttachPin(PIN_LCD_BL, 0);
-  // for (uint8_t i = 0; i < 0xFF; i++) {
-  //   ledcWrite(0, i);
-  //   delay(2);
-  // }
-  // delay(1000);
 
   button.attachClick(
       [](void *param) {
