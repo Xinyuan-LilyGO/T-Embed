3c39f401c96e0765daada1668b474b60f3a8e983
lewishe <lewishe@outlook.com>
2023-03-18 10:09:52 +0800

    Factory program adaptation to Arduino IDE

--- example/factory/src/alibaba_font.c
+++ example/factory/src/alibaba_font.c
@@ -4,7 +4,7 @@
  * Opts: 
  ******************************************************************************/
 
-#ifdef LV_LVGL_H_INCLUDE_SIMPLE
+#ifdef LV_CONF_INCLUDE_SIMPLE
 #include "lvgl.h"
 #else
 #include "lvgl/lvgl.h"
