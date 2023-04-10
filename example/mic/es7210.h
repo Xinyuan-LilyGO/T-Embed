6ed04d8099ba0e7ba5985a07e4c7d0f9666083fe
lewishe <lewishe@outlook.com>
2023-04-10 09:19:02 +0800

    Update micphone example

--- example/mic/es7210.h
+++ example/mic/es7210.h
@@ -1,173 +1,260 @@
-#pragma once
+/*
+ * ESPRESSIF MIT License
+ *
+ * Copyright (c) 2021 <ESPRESSIF SYSTEMS (SHANGHAI) CO., LTD>
+ *
+ * Permission is hereby granted for use on all ESPRESSIF SYSTEMS products, in which case,
+ * it is free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
 
-#include "Wire.h"
+#ifndef _ES7210_H
+#define _ES7210_H
+
 #include "audio_hal.h"
-#include "driver/i2s.h"
-
-#define ES7210_RESET_REG00           0x00 /* Reset control */
-#define ES7210_CLOCK_OFF_REG01       0x01 /* Used to turn off the ADC clock */
-#define ES7210_MAINCLK_REG02         0x02 /* Set ADC clock frequency division */
-#define ES7210_MASTER_CLK_REG03      0x03 /* MCLK source $ SCLK division */
-#define ES7210_LRCK_DIVH_REG04       0x04 /* lrck_divh */
-#define ES7210_LRCK_DIVL_REG05       0x05 /* lrck_divl */
-#define ES7210_POWER_DOWN_REG06      0x06 /* power down */
-#define ES7210_OSR_REG07             0x07
-#define ES7210_MODE_CONFIG_REG08     0x08 /* Set master/slave & channels */
-#define ES7210_TIME_CONTROL0_REG09   0x09 /* Set Chip intial state period*/
-#define ES7210_TIME_CONTROL1_REG0A   0x0A /* Set Power up state period */
-#define ES7210_SDP_INTERFACE1_REG11  0x11 /* Set sample & fmt */
-#define ES7210_SDP_INTERFACE2_REG12  0x12 /* Pins state */
-#define ES7210_ADC_AUTOMUTE_REG13    0x13 /* Set mute */
-#define ES7210_ADC34_MUTERANGE_REG14 0x14 /* Set mute range */
-#define ES7210_ADC34_HPF2_REG20      0x20 /* HPF */
-#define ES7210_ADC34_HPF1_REG21      0x21
-#define ES7210_ADC12_HPF1_REG22      0x22
-#define ES7210_ADC12_HPF2_REG23      0x23
-#define ES7210_ANALOG_REG40          0x40 /* ANALOG Power */
-#define ES7210_MIC12_BIAS_REG41      0x41
-#define ES7210_MIC34_BIAS_REG42      0x42
-#define ES7210_MIC1_GAIN_REG43       0x43
-#define ES7210_MIC2_GAIN_REG44       0x44
-#define ES7210_MIC3_GAIN_REG45       0x45
-#define ES7210_MIC4_GAIN_REG46       0x46
-#define ES7210_MIC1_POWER_REG47      0x47
-#define ES7210_MIC2_POWER_REG48      0x48
-#define ES7210_MIC3_POWER_REG49      0x49
-#define ES7210_MIC4_POWER_REG4A      0x4A
-#define ES7210_MIC12_POWER_REG4B     0x4B /* MICBias & ADC & PGA Power */
-#define ES7210_MIC34_POWER_REG4C     0x4C
+#include <Wire.h>
 
 typedef enum {
-  ES7210_AD1_AD0_00 = 0x40,
-  ES7210_AD1_AD0_01 = 0x41,
-  ES7210_AD1_AD0_10 = 0x42,
-  ES7210_AD1_AD0_11 = 0x43,
+    ES7210_AD1_AD0_00 = 0x40,
+    ES7210_AD1_AD0_01 = 0x41,
+    ES7210_AD1_AD0_10 = 0x42,
+    ES7210_AD1_AD0_11 = 0x43,
 } es7210_address_t;
 
-typedef enum { ES7210_INPUT_MIC1 = 0x01, ES7210_INPUT_MIC2 = 0x02, ES7210_INPUT_MIC3 = 0x04, ES7210_INPUT_MIC4 = 0x08 } es7210_input_mics_t;
+/* ES7210 address*/
+#define ES7210_ADDR                   ES7210_AD1_AD0_00
 
-typedef enum gain_value {
-  GAIN_0DB = 0,
-  GAIN_3DB,
-  GAIN_6DB,
-  GAIN_9DB,
-  GAIN_12DB,
-  GAIN_15DB,
-  GAIN_18DB,
-  GAIN_21DB,
-  GAIN_24DB,
-  GAIN_27DB,
-  GAIN_30DB,
-  GAIN_33DB,
-  GAIN_34_5DB,
-  GAIN_36DB,
-  GAIN_37_5DB,
-} es7210_gain_value_t;
-
-#define ES7210_DEBUG 1
-#if ES7210_DEBUG
-#define DEBUG_PRINTF(format, ...) printf(format, ##__VA_ARGS__)
-#elif
-#define DEBUG_PRINTF(format, ...) ;
+#ifdef __cplusplus
+extern "C" {
 #endif
 
-struct _coeff_div {
-  uint32_t mclk; /* mclk frequency */
-  uint32_t lrck; /* lrck */
-  uint8_t ss_ds;
-  uint8_t adc_div;  /* adcclk divider */
-  uint8_t dll;      /* dll_bypass */
-  uint8_t doubler;  /* doubler enable */
-  uint8_t osr;      /* adc osr */
-  uint8_t mclk_src; /* select mclk  source */
-  uint32_t lrck_h;  /* The high 4 bits of lrck */
-  uint32_t lrck_l;  /* The low 8 bits of lrck */
-};
+#define  ES7210_RESET_REG00                 0x00        /* Reset control */
+#define  ES7210_CLOCK_OFF_REG01             0x01        /* Used to turn off the ADC clock */
+#define  ES7210_MAINCLK_REG02               0x02        /* Set ADC clock frequency division */
+#define  ES7210_MASTER_CLK_REG03            0x03        /* MCLK source $ SCLK division */
+#define  ES7210_LRCK_DIVH_REG04             0x04        /* lrck_divh */
+#define  ES7210_LRCK_DIVL_REG05             0x05        /* lrck_divl */
+#define  ES7210_POWER_DOWN_REG06            0x06        /* power down */
+#define  ES7210_OSR_REG07                   0x07
+#define  ES7210_MODE_CONFIG_REG08           0x08        /* Set master/slave & channels */
+#define  ES7210_TIME_CONTROL0_REG09         0x09        /* Set Chip intial state period*/
+#define  ES7210_TIME_CONTROL1_REG0A         0x0A        /* Set Power up state period */
+#define  ES7210_SDP_INTERFACE1_REG11        0x11        /* Set sample & fmt */
+#define  ES7210_SDP_INTERFACE2_REG12        0x12        /* Pins state */
+#define  ES7210_ADC_AUTOMUTE_REG13          0x13        /* Set mute */
+#define  ES7210_ADC34_MUTERANGE_REG14       0x14        /* Set mute range */
+#define  ES7210_ADC34_HPF2_REG20            0x20        /* HPF */
+#define  ES7210_ADC34_HPF1_REG21            0x21
+#define  ES7210_ADC12_HPF1_REG22            0x22
+#define  ES7210_ADC12_HPF2_REG23            0x23
+#define  ES7210_ANALOG_REG40                0x40        /* ANALOG Power */
+#define  ES7210_MIC12_BIAS_REG41            0x41
+#define  ES7210_MIC34_BIAS_REG42            0x42
+#define  ES7210_MIC1_GAIN_REG43             0x43
+#define  ES7210_MIC2_GAIN_REG44             0x44
+#define  ES7210_MIC3_GAIN_REG45             0x45
+#define  ES7210_MIC4_GAIN_REG46             0x46
+#define  ES7210_MIC1_POWER_REG47            0x47
+#define  ES7210_MIC2_POWER_REG48            0x48
+#define  ES7210_MIC3_POWER_REG49            0x49
+#define  ES7210_MIC4_POWER_REG4A            0x4A
+#define  ES7210_MIC12_POWER_REG4B           0x4B        /* MICBias & ADC & PGA Power */
+#define  ES7210_MIC34_POWER_REG4C           0x4C
 
-static const struct _coeff_div coeff_div[] = {
-    // mclk      lrck    ss_ds adc_div  dll  doubler osr  mclk_src  lrckh   lrckl
-    /* 8k */
-    {12288000, 8000, 0x00, 0x03, 0x01, 0x00, 0x20, 0x00, 0x06, 0x00},
-    {16384000, 8000, 0x00, 0x04, 0x01, 0x00, 0x20, 0x00, 0x08, 0x00},
-    {19200000, 8000, 0x00, 0x1e, 0x00, 0x01, 0x28, 0x00, 0x09, 0x60},
-    {4096000, 8000, 0x00, 0x01, 0x01, 0x00, 0x20, 0x00, 0x02, 0x00},
 
-    /* 11.025k */
-    {11289600, 11025, 0x00, 0x02, 0x01, 0x00, 0x20, 0x00, 0x01, 0x00},
 
-    /* 12k */
-    {12288000, 12000, 0x00, 0x02, 0x01, 0x00, 0x20, 0x00, 0x04, 0x00},
-    {19200000, 12000, 0x00, 0x14, 0x00, 0x01, 0x28, 0x00, 0x06, 0x40},
+typedef enum {
+    ES7210_INPUT_MIC1 = 0x01,
+    ES7210_INPUT_MIC2 = 0x02,
+    ES7210_INPUT_MIC3 = 0x04,
+    ES7210_INPUT_MIC4 = 0x08
+} es7210_input_mics_t;
 
-    /* 16k */
-    {4096000, 16000, 0x00, 0x01, 0x01, 0x01, 0x20, 0x00, 0x01, 0x00},
-    {19200000, 16000, 0x00, 0x0a, 0x00, 0x00, 0x1e, 0x00, 0x04, 0x80},
-    {16384000, 16000, 0x00, 0x02, 0x01, 0x00, 0x20, 0x00, 0x04, 0x00},
-    {12288000, 16000, 0x00, 0x03, 0x01, 0x01, 0x20, 0x00, 0x03, 0x00},
+typedef enum gain_value{
+	GAIN_0DB = 0,
+	GAIN_3DB,
+	GAIN_6DB,
+	GAIN_9DB,
+	GAIN_12DB,
+	GAIN_15DB,
+	GAIN_18DB,
+	GAIN_21DB,
+	GAIN_24DB,
+	GAIN_27DB,
+	GAIN_30DB,
+	GAIN_33DB,
+	GAIN_34_5DB,
+	GAIN_36DB,
+	GAIN_37_5DB,
+} es7210_gain_value_t;
 
-    /* 22.05k */
-    {11289600, 22050, 0x00, 0x01, 0x01, 0x00, 0x20, 0x00, 0x02, 0x00},
+/*
+ * @brief Initialize ES7210 ADC chip
+ *
+ * @param[in] codec_cfg:  configuration of ES7210
+ *
+ * @return
+ *      - ESP_OK
+ *      - ESP_FAIL
+ */
+esp_err_t es7210_adc_init(TwoWire *tw, audio_hal_codec_config_t *codec_cfg);
 
-    /* 24k */
-    {12288000, 24000, 0x00, 0x01, 0x01, 0x00, 0x20, 0x00, 0x02, 0x00},
-    {19200000, 24000, 0x00, 0x0a, 0x00, 0x01, 0x28, 0x00, 0x03, 0x20},
+/**
+ * @brief Deinitialize ES7210 ADC chip
+ *
+ * @return
+ *     - ESP_OK
+ *     - ESP_FAIL
+ */
+esp_err_t es7210_adc_deinit();
 
-    /* 32k */
-    {12288000, 32000, 0x00, 0x03, 0x00, 0x00, 0x20, 0x00, 0x01, 0x80},
-    {16384000, 32000, 0x00, 0x01, 0x01, 0x00, 0x20, 0x00, 0x02, 0x00},
-    {19200000, 32000, 0x00, 0x05, 0x00, 0x00, 0x1e, 0x00, 0x02, 0x58},
+/**
+ * @brief Configure ES7210 ADC mode and I2S interface
+ *
+ * @param[in] mode:  codec mode
+ * @param[in] iface:  I2S config
+ *
+ * @return
+ *     - ESP_FAIL Parameter error
+ *     - ESP_OK   Success
+ */
+esp_err_t es7210_adc_config_i2s(audio_hal_codec_mode_t mode, audio_hal_codec_i2s_iface_t *iface);
 
-    /* 44.1k */
-    {11289600, 44100, 0x00, 0x01, 0x01, 0x01, 0x20, 0x00, 0x01, 0x00},
+/**
+ * @brief Control ES7210 ADC chip
+ *
+ * @param[in] mode:  codec mode
+ * @param[in] ctrl_state:  start or stop progress
+ *
+ * @return
+ *     - ESP_FAIL Parameter error
+ *     - ESP_OK   Success
+ */
+esp_err_t es7210_adc_ctrl_state(audio_hal_codec_mode_t mode, audio_hal_ctrl_t ctrl_state);
 
-    /* 48k */
-    {12288000, 48000, 0x00, 0x01, 0x01, 0x01, 0x20, 0x00, 0x01, 0x00},
-    {19200000, 48000, 0x00, 0x05, 0x00, 0x01, 0x28, 0x00, 0x01, 0x90},
+/**
+ * @brief  Set gain of given mask
+ *
+ * @param[in] mic_mask Mask of MIC channel
+ *
+ * @param[in] gain:  gain
+ *
+ *       gain        :   value
+ *       GAIN_0DB    :   1
+ *       GAIN_3DB    :   2
+ *       GAIN_6DB    :   3
+ *           ·
+ *           ·
+ *           ·
+ *       GAIN_30DB   :   10
+ *       GAIN_33DB   :   11
+ *       GAIN_34_5DB :   12
+ *       GAIN_36DB   :   13
+ *       GAIN_37_5DB :   14
+ *
+ * @return
+ *     - ESP_OK
+ *     - ESP_FAIL
+ */
+esp_err_t es7210_adc_set_gain(es7210_input_mics_t mic_mask, es7210_gain_value_t gain);
 
-    /* 64k */
-    {16384000, 64000, 0x01, 0x01, 0x01, 0x00, 0x20, 0x00, 0x01, 0x00},
-    {19200000, 64000, 0x00, 0x05, 0x00, 0x01, 0x1e, 0x00, 0x01, 0x2c},
+/**
+ * @brief  Set gain (Note: the enabled microphone sets the same gain)
+ *
+ * @param[in] gain:  gain
+ *
+ *       gain        :   value
+ *       GAIN_0DB    :   1
+ *       GAIN_3DB    :   2
+ *       GAIN_6DB    :   3
+ *           ·
+ *           ·
+ *           ·
+ *       GAIN_30DB   :   10
+ *       GAIN_33DB   :   11
+ *       GAIN_34_5DB :   12
+ *       GAIN_36DB   :   13
+ *       GAIN_37_5DB :   14
+ *
+ * @return
+ *     - ESP_OK
+ *     - ESP_FAIL
+ */
+esp_err_t es7210_adc_set_gain_all(es7210_gain_value_t gain);
 
-    /* 88.2k */
-    {11289600, 88200, 0x01, 0x01, 0x01, 0x01, 0x20, 0x00, 0x00, 0x80},
+/**
+ * @brief Get MIC gain
+ *
+ * @param mic_mask Selected MIC
+ * @param gain Pointer to `es7210_gain_value_t`
+ * @return
+ *     - ESP_OK
+ *     - ESP_FAIL
+ */
+esp_err_t es7210_adc_get_gain(es7210_input_mics_t mic_mask, es7210_gain_value_t *gain);
 
-    /* 96k */
-    {12288000, 96000, 0x01, 0x01, 0x01, 0x01, 0x20, 0x00, 0x00, 0x80},
-    {19200000, 96000, 0x01, 0x05, 0x00, 0x01, 0x28, 0x00, 0x00, 0xc8},
-};
+/**
+ * @brief Set volume
+ *
+ * @param[in] volume:  volume
+ *
+ * @return
+ *     - ESP_OK
+ */
+esp_err_t es7210_adc_set_volume(int volume);
 
-class ES7210 {
-public:
-  ES7210(es7210_address_t address);
-  bool begin(TwoWire *wire = &Wire);
-  bool mic_select(es7210_input_mics_t mic);
-  void adc_config_i2s(audio_hal_codec_mode_t mode, audio_hal_codec_i2s_iface_t *iface);
-  void set_bits(audio_hal_iface_bits_t bits);
-  int get_coeff(uint32_t mclk, uint32_t lrck);
-  void config_fmt(audio_hal_iface_format_t fmt);
-  void config_sample(audio_hal_iface_samples_t sample);
-  void adc_ctrl_state(audio_hal_codec_mode_t mode, audio_hal_ctrl_t ctrl_state);
+/**
+ * @brief Set ES7210 ADC mute status
+ *
+ * @return
+ *     - ESP_FAIL
+ *     - ESP_OK
+ */
+esp_err_t es7210_set_mute(bool enable);
 
-  void start(uint8_t clock_reg_value);
-  void stop();
-  void adc_set_gain(es7210_input_mics_t mic_mask, es7210_gain_value_t gain);
-  void adc_set_gain_all(es7210_gain_value_t gain);
+/**
+ * @brief Select ES7210 mic
+ *
+ * @param[in] mic:  mics
+ *
+ * @return
+ *     - ESP_FAIL
+ *     - ESP_OK
+ */
+esp_err_t es7210_mic_select(es7210_input_mics_t mic);
 
-  void read_all_reg(void) {
-    for (int i = 0; i <= 0x4E; i++) {
-      uint8_t reg = readRegister(i);
-      printf("REG:%02x, %02x\n", reg, i);
-    }
-  }
+/**
+ * @brief Read regs of ES7210
+ *
+ * @param[in] reg_addr:  reg_addr
+ *
+ * @return
+ *     - ESP_FAIL
+ *     - ESP_OK
+ */
+int es7210_read_reg(uint8_t reg_addr);
 
-protected:
-  bool writeRegister(uint8_t reg, uint8_t value);
-  uint8_t readRegister(uint8_t reg);
-  bool update_reg_bit(uint8_t reg_addr, uint8_t update_bits, uint8_t data);
+/**
+ * @brief Read all regs of ES7210
+ */
+void es7210_read_all(void);
 
-  es7210_input_mics_t _mic_select ;
+#ifdef __cplusplus
+}
+#endif
 
-  uint8_t _address;
-  TwoWire *_wire;
-  uint32_t _error;
-};
+#endif /* _ES7210_H_ */
--- example/mic/mic.ino
+++ example/mic/mic.ino
@@ -1,61 +1,101 @@
-#include "Arduino.h"
-#include "driver/i2s.h"
+#include <Arduino.h>
+#include <driver/i2s.h>
+#include <esp_vad.h>
 #include "es7210.h"
 #include "pin_config.h"
 
-ES7210 mic(ES7210_AD1_AD0_00);
-#define SAMPLES      512
-#define SAMPLE_BLOCK 64
-#define SAMPLE_FREQ  16000
+#define VAD_SAMPLE_RATE_HZ              16000
+#define VAD_FRAME_LENGTH_MS             30
+#define VAD_BUFFER_LENGTH               (VAD_FRAME_LENGTH_MS * VAD_SAMPLE_RATE_HZ / 1000)
+#define I2S_CH                          I2S_NUM_1
 
-void setup() {
-  pinMode(PIN_POWER_ON, OUTPUT);
-  digitalWrite(PIN_POWER_ON, HIGH);
-  Serial.begin(115200);
-  Serial.printf("psram size : %d kb\r\n", ESP.getPsramSize() / 1024);
-  Serial.printf("FLASH size : %d kb\r\n", ESP.getFlashChipSize() / 1024);
+int16_t         *vad_buff;
+vad_handle_t    vad_inst;
+size_t          bytes_read;
 
-  Wire.begin(PIN_IIC_SDA, PIN_IIC_SCL);
-  mic.begin(&Wire);
-  audio_hal_codec_i2s_iface_t i2s_cfg;
-  i2s_cfg.bits = AUDIO_HAL_BIT_LENGTH_16BITS;
-  i2s_cfg.fmt = AUDIO_HAL_I2S_NORMAL;
-  i2s_cfg.mode = AUDIO_HAL_MODE_SLAVE;
-  i2s_cfg.samples = AUDIO_HAL_16K_SAMPLES;
-  mic.adc_config_i2s(AUDIO_HAL_CODEC_MODE_ENCODE, &i2s_cfg);
-  mic.mic_select((es7210_input_mics_t)(ES7210_INPUT_MIC1 | ES7210_INPUT_MIC2));
-  mic.adc_set_gain((es7210_input_mics_t)(ES7210_INPUT_MIC1 | ES7210_INPUT_MIC2), GAIN_9DB);
-  mic.adc_ctrl_state(AUDIO_HAL_CODEC_MODE_ENCODE, AUDIO_HAL_CTRL_START);
+void setup()
+{
+    pinMode(PIN_POWER_ON, OUTPUT);
+    digitalWrite(PIN_POWER_ON, HIGH);
 
-  i2s_config_t i2s_config = {
-      .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
-      .sample_rate = SAMPLE_FREQ,
-      .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
-      .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
-      .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_I2S),
-      .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-      .dma_buf_count = 8,
-      .dma_buf_len = SAMPLE_BLOCK,
-      .use_apll = false,
-      .tx_desc_auto_clear = true,
-      .fixed_mclk = 0,
-      .mclk_multiple = I2S_MCLK_MULTIPLE_256,
-      .bits_per_chan = I2S_BITS_PER_CHAN_16BIT,
-  };
-  i2s_pin_config_t pin_config = {0};
-  pin_config.bck_io_num = PIN_ES7210_BCLK;
-  pin_config.ws_io_num = PIN_ES7210_LRCK;
-  pin_config.data_in_num = PIN_ES7210_DIN;
-  pin_config.mck_io_num = PIN_ES7210_MCLK;
-  i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
-  i2s_set_pin(I2S_NUM_0, &pin_config);
-  i2s_zero_dma_buffer(I2S_NUM_0);
-  // i2s_start(I2S_NUM_1);
+    Serial.begin(115200);
+    Serial.printf("psram size : %d kb\r\n", ESP.getPsramSize() / 1024);
+    Serial.printf("FLASH size : %d kb\r\n", ESP.getFlashChipSize() / 1024);
+
+    Wire.begin(PIN_IIC_SDA, PIN_IIC_SCL);
+
+    uint32_t ret_val = ESP_OK;
+
+    audio_hal_codec_config_t cfg = {
+        .adc_input = AUDIO_HAL_ADC_INPUT_ALL,
+        .codec_mode = AUDIO_HAL_CODEC_MODE_ENCODE,
+        .i2s_iface =
+        {
+            .mode = AUDIO_HAL_MODE_SLAVE,
+            .fmt = AUDIO_HAL_I2S_NORMAL,
+            .samples = AUDIO_HAL_16K_SAMPLES,
+            .bits = AUDIO_HAL_BIT_LENGTH_16BITS,
+        },
+    };
+
+    ret_val |= es7210_adc_init(&Wire, &cfg);
+    ret_val |= es7210_adc_config_i2s(cfg.codec_mode, &cfg.i2s_iface);
+    ret_val |= es7210_adc_set_gain(
+                   (es7210_input_mics_t)(ES7210_INPUT_MIC1 | ES7210_INPUT_MIC2),
+                   (es7210_gain_value_t)GAIN_0DB);
+    ret_val |= es7210_adc_set_gain(
+                   (es7210_input_mics_t)(ES7210_INPUT_MIC3 | ES7210_INPUT_MIC4),
+                   (es7210_gain_value_t)GAIN_37_5DB);
+    ret_val |= es7210_adc_ctrl_state(cfg.codec_mode, AUDIO_HAL_CTRL_START);
+
+
+
+    i2s_config_t i2s_config = {
+        .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
+        .sample_rate = VAD_SAMPLE_RATE_HZ,
+        .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+        .channel_format = I2S_CHANNEL_FMT_ALL_LEFT,
+        .communication_format = I2S_COMM_FORMAT_STAND_I2S,
+        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+        .dma_buf_count = 8,
+        .dma_buf_len = 64,
+        .use_apll = false,
+        .tx_desc_auto_clear = true,
+        .fixed_mclk = 0,
+        .mclk_multiple = I2S_MCLK_MULTIPLE_256,
+        .bits_per_chan = I2S_BITS_PER_CHAN_16BIT,
+        .chan_mask = (i2s_channel_t)(I2S_TDM_ACTIVE_CH0 | I2S_TDM_ACTIVE_CH1),
+    };
+
+    i2s_pin_config_t pin_config = {0};
+    pin_config.bck_io_num = PIN_ES7210_BCLK;
+    pin_config.ws_io_num = PIN_ES7210_LRCK;
+    pin_config.data_in_num = PIN_ES7210_DIN;
+    pin_config.mck_io_num = PIN_ES7210_MCLK;
+    i2s_driver_install(I2S_CH, &i2s_config, 0, NULL);
+    i2s_set_pin(I2S_CH, &pin_config);
+    i2s_zero_dma_buffer(I2S_CH);
+    // i2s_start(I2S_NUM_1);
+
+
+    vad_inst = vad_create(VAD_MODE_0);
+    vad_buff = (int16_t *)malloc(VAD_BUFFER_LENGTH * sizeof(short));
+    if (vad_buff == NULL) {
+        while (1) {
+            Serial.println("Memory allocation failed!");
+            delay(1000);
+        }
+    }
 }
 
-void loop() {
-  size_t bytes_read;
-  uint16_t buffer[3200] = {0};
-  delay(10);
-  i2s_read(I2S_NUM_0, &buffer, sizeof(buffer), &bytes_read, 15);
+void loop()
+{
+    i2s_read(I2S_CH, (char *)vad_buff, VAD_BUFFER_LENGTH * sizeof(short), &bytes_read, portMAX_DELAY);
+    // Feed samples to the VAD process and get the result
+    vad_state_t vad_state = vad_process(vad_inst, vad_buff, VAD_SAMPLE_RATE_HZ, VAD_FRAME_LENGTH_MS);
+    if (vad_state == VAD_SPEECH) {
+        Serial.print(millis());
+        Serial.println("Speech detected");
+    }
+    delay(5);
 }
