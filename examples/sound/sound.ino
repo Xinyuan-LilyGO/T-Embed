a2c20fd805a93ed24368ed2636e0028cb72d07ec
lewishe <lewishe@outlook.com>
2023-10-18 17:48:53 +0800

    Update sound example https://github.com/Xinyuan-LilyGO/T-Embed/issues/29

--- examples/sound/sound.ino
+++ examples/sound/sound.ino
@@ -13,11 +13,21 @@ void setup() {
   Serial.printf("psram size : %d kb\r\n", ESP.getPsramSize() / 1024);
   Serial.printf("FLASH size : %d kb\r\n", ESP.getFlashChipSize() / 1024);
 
-  SPIFFS.begin(true);
+  //  *     ArduinoIDE only 1.8.19 - upload SPIFFS data with ESP32 Sketch Data Upload:
+  //  *     ESP32: https://github.com/lorol/arduino-esp32fs-plugin
+  //  *     Platformio
+  // *      ENV -> sound -> Platform -> Upload Filesystem Image 
+  bool ret = SPIFFS.begin();
+  while(!ret){
+    Serial.println("SPIFFS not mount . please Upload mp3 file");delay(1000);
+  }
   audio = new Audio(0, 3, 1);
   audio->setPinout(PIN_IIS_BCLK, PIN_IIS_WCLK, PIN_IIS_DOUT);
   audio->setVolume(21); // 0...21
-  audio->connecttoFS(SPIFFS, "/sound1.mp3");
+  if(!audio->connecttoFS(SPIFFS, "/sound1.mp3")){
+    Serial.println("No find sound1.mp3.");while(1)delay(1000);
+  }
+  Serial.println("Now play sound ...");
 }
 
 void loop() {
